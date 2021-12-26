5327843d2f89862380ad29761a020cb59e8cb4b2
Vinicius  Pereira <viniciuspoa2@hotmail.com>
2021-12-26 15:41:28 -0300

    Update contentScript.ts

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -4,15 +4,24 @@ const rules:{
     [url:string]:() => void
 } =  {
     "https://www.nytimes.com/section/technology":filterNYTTechnology,
-}
+};
 
 function filterNYTTechnology(){
     const app = document.getElementById('site-content');
     const wrapper = document.getElementById("top-wrapper");
     app.removeChild(wrapper);
-}
+};
 
+
+function  filterNYTTechnology2(){
+    const divs = document.getElementsByTagName("div");
+    for(const div of divs){
+        if(div.className.indexOf('ad') != -1){
+            div.style.display = 'none'
+        }
+    }
+};
 if(document.URL in rules){
     console.log(document.URL);
     rules[document.URL]()
-}
+};
